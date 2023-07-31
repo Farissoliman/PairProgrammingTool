a1c2afe89f95cfaf7ea8a8107253acb66944aa5f
Faris Soliman <fasolima@ncsu.edu>
2023-07-31 07:35:46 -0400

    Pie charts made for end

--- src/app/end/page.tsx
+++ src/app/end/page.tsx
@@ -2,6 +2,7 @@
 
 import { sum } from "@/utils";
 import { getUID, getPartnerUID, useAutoRerender, useStats } from "@/utils/react";
+import { PieChart } from "@/components/pieChart";
 
 import driverMinutes from "../stats/page";
 import navigatorMinutes from "../stats/page";
@@ -51,7 +52,6 @@ export default function Page() {
   }
 
   const nonVerbalExpressions = lastInterval.emotions.length;
-  const totalExpressions = nonVerbalExpressions + userUtterances;
 
   const linesOfCode = lastInterval.keystrokes;
   // const partnerLinesOfCode = partnerData.intervals[partnerData.intervals.length - 1].keystrokes;
@@ -80,48 +80,54 @@ export default function Page() {
             </li>
           </ul>
           <div className="flex pt-4">
-            <div className="flex-1 text-xl">
+            <div className="flex-1 text-2xl m-auto">
               <p className="mb-0">
                 Lines of code
               </p>
-              <p className="text-sm mt-3">
+              <p className="text-sm mt-3 opacity-60">
                 Percentage of the total lines of code written by you and your partner
               </p>
             </div>
-            <div className="flex-2">Pie Chart Goes Here</div>
+            {/* <PieChart data={[{ label : "You", value : linesOfCode }, { label : "Partner", value : partnerLinesOfCode }]} /> */}
           </div>
           <div className="flex pt-4">
-            <div className="flex-1 text-xl">
+            <div className="flex-1 text-2xl m-auto">
               <p className="mb-0">
                 Communication Style
               </p>
-              <p className="text-sm mt-3">
-                Verbal communication represents the number of utterances spoken by you.
-                Non-Verbal represents the number of facial expressions made by you.
+              <p className="text-sm mt-3 opacity-60">
+                Number of utterances spoken by you (verbal) vs 
+                Number of facial expressions (non-verbal)
               </p>
             </div>
-            <div className="flex-2">Pie Chart Goes Here</div>
+            <PieChart 
+              data={[{ label : "Verbal", value : userUtterances }, { label : "Non-Verbal", value : nonVerbalExpressions }]}
+              outerRadius={100}
+              innerRadius={0}
+            />
           </div>
           <div className="flex pt-4">
-            <div className="flex-1 text-xl">
+            <div className="flex-1 text-2xl m-auto">
               <p className="mb-0">
                 Interruptions
               </p>
-              <p className="text-sm mt-3">
-                Number of times you interrupted your partner as a percentage of total interruptions
+              <p className="text-sm mt-3 opacity-60">
+                Number of times you interrupted your partner and the number of times they interrupted you.
               </p>
             </div>
+            {/* <PieChart data={[{ label : "You", value : interruptions }, { label : "Partner", value : partnerInterruptions }]} /> */}
             <div className="flex-2">Pie Chart Goes Here</div>
           </div>
           <div className="flex pt-4">
-            <div className="flex-1 text-xl">
+            <div className="flex-1 text-2xl m-auto">
               <p className="mb-0">
                 Leadership Style
               </p>
-              <p className="text-sm mt-3">
+              <p className="text-sm mt-3 opacity-60">
                 Were the number of total lines of code written by you and your partner equal?
               </p>
             </div>
+            {/* <PieChart data={[{ label : "You", value : linesOfCode }, { label : "Partner", value : partnerLinesOfCode }]} /> */}
             <div className="flex-2">Pie Chart Goes Here</div>
           </div>
         </div>
